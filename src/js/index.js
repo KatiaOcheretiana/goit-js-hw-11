e6a59891d9708853e38cdc06ae2a7846e7c82d56
KatiaOcheretiana <130676842+KatiaOcheretiana@users.noreply.github.com>
2023-10-01 12:53:36 +0300

    task is done

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,5 @@
 import { selectors } from './selectors';
-import { onSubmitSearch, onLoadMore } from "./functions"
-// import SimpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
-
-// const lightbox = new SimpleLightbox('.gallery a');
-
-
+import { onSubmitSearch, onLoadMore } from "./functions";
 
 
 selectors.loadMore.classList.add('hidden')
@@ -13,7 +7,5 @@ selectors.loadMore.classList.add('hidden')
 selectors.form.addEventListener('submit', onSubmitSearch)
 
 selectors.loadMore.addEventListener("click", onLoadMore)
-
-
 
 
